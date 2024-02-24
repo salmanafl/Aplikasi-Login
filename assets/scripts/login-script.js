23dd70a1f629c7808a544f66f1f97270f79a84d7
magistra nouvelle <142574389+salmanafl@users.noreply.github.com>
2024-02-25 05:31:49 +0700

    Update login-script.js
    ganti komentar sesuai format jsdoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,54 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+/** 
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
 
+/* Comment :  Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /* Comment :  Menambahkan aksi klik pada button. */
   const email = inputEmailElement.value;
+
+  /** 
+     * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
+     * @constant {string}
+     */
   const password = inputPasswordElement.value;
   if (email == expectedEmail && password == expectedPassword) {
+    /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
     goToHome();
   } else {
+    /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
   }
 });
